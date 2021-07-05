ec0bd4f509d252d803cb758da8c2ae0f6958d65b
Taylan Gökkaya <insomnimus.uni@gmail.com>
2021-07-05 18:18:17 +0300

    '[minor]: added --follow-links flag and improved performance (up to 10x on windows)'

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,70 +1,13 @@
-use clap::{crate_version, App, Arg};
-use std::io::ErrorKind;
-use std::{fs, process};
-
+use clap::{crate_version, App, AppSettings, Arg};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process,
+};
 use walkdir::WalkDir;
-
-fn calc_dir(dir: &str, ignore_hidden: bool) -> u64 {
-    if ignore_hidden && is_hidden(dir) {
-        return 0;
-    }
-    if ignore_hidden {
-        WalkDir::new(dir)
-            .into_iter()
-            .filter_entry(|x| {
-                x.file_name()
-                    .to_str()
-                    .map(|s| !is_hidden(s))
-                    .unwrap_or(false)
-            })
-            .filter_map(|e| e.ok())
-            .filter(|x| !x.file_type().is_dir())
-            .map(|x| {
-                x.path()
-                    .symlink_metadata()
-                    .map(|md| md.len())
-                    .unwrap_or(0_u64)
-            })
-            .sum()
-    } else {
-        WalkDir::new(dir)
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|x| !x.file_type().is_dir())
-            .map(|x| {
-                x.path()
-                    .symlink_metadata()
-                    .map(|md| md.len())
-                    .unwrap_or(0_u64)
-            })
-            .sum()
-    }
-}
-
-fn calc_size(name: &str, ignore_hidden: bool) -> u64 {
-    if ignore_hidden && is_hidden(name) {
-        return 0;
-    }
-    match fs::symlink_metadata(name) {
-        Ok(m) => {
-            if m.is_dir() {
-                calc_dir(name, ignore_hidden)
-            } else {
-                m.len()
-            }
-        }
-        Err(err) => match err.kind() {
-            ErrorKind::NotFound => {
-                eprintln!("{}: does not exist", name);
-                process::exit(1);
-            }
-            _ => 0,
-        },
-    }
-}
-
 pub struct Cmd {
     ignore_hidden: bool,
+    follow_links: bool,
     pub bytes: bool,
     files: Vec<String>,
 }
@@ -73,16 +16,8 @@ impl Cmd {
     pub fn app() -> App<'static> {
         let app = App::new("sz")
             .about("calculates file sizes")
-            .override_usage("sz [OPTIONS] [FILE...]")
             .version(crate_version!())
-            .help_template(
-                "\
-sz, {about}
-usage: {usage}
-
-{all-args}\
-	",
-            );
+            .setting(AppSettings::UnifiedHelpMessage);
 
         let bytes = Arg::new("bytes")
             .short('b')
@@ -96,12 +31,21 @@ usage: {usage}
 
         let file = Arg::new("file").multiple(true);
 
-        app.arg(bytes).arg(ignore_hidden).arg(file)
+        let follow_links = Arg::new("follow-links")
+            .short('f')
+            .long("follow-links")
+            .about("follow links");
+
+        app.arg(follow_links)
+            .arg(bytes)
+            .arg(ignore_hidden)
+            .arg(file)
     }
 
     pub fn from_args() -> Self {
         let matches = Self::app().get_matches();
 
+        let follow_links = matches.is_present("follow-links");
         let bytes = matches.is_present("bytes");
         let ignore_hidden = matches.is_present("ignore-hidden");
 
@@ -111,6 +55,7 @@ usage: {usage}
         };
         Self {
             ignore_hidden,
+            follow_links,
             bytes,
             files,
         }
@@ -120,64 +65,97 @@ usage: {usage}
     #[cfg(windows)]
     pub fn calculate(&self) -> u64 {
         use glob::{glob_with, MatchOptions};
-        use itertools::Itertools;
 
         if self.files.is_empty() {
-            return calc_dir("./", self.ignore_hidden);
+            return self.calc_dir("./");
         }
 
-        let mut files: Vec<String> = vec![];
         let opt = MatchOptions {
             case_sensitive: false,
             require_literal_separator: true,
             require_literal_leading_dot: self.ignore_hidden,
         };
 
-        for s in &self.files {
-            if s.contains('*') || s.contains('[') || s.contains('?') {
-                for result in glob_with(&s[..], opt)
-                    .unwrap_or_else(|e| {
-                        eprintln!("failed to read the glob pattern: {:?}", e);
-                        process::exit(1);
-                    })
-                    .filter_map(|x| x.ok())
-                    .map(|x| x.into_os_string().into_string())
-                    .filter_map(|e| e.ok())
-                {
-                    files.push(result)
-                }
-                continue;
-            }
-            files.push(s.to_string());
-        }
-
-        if files.is_empty() {
-            return 0;
-        }
-
-        files
+        self.files
             .iter()
-            .unique()
-            .map(|x| calc_size(&x[..], self.ignore_hidden))
+            .map(|s| {
+                if is_glob(s) {
+                    glob_with(s, opt)
+                        .unwrap_or_else(|e| {
+                            eprintln!("failed to read the glob pattern: {:?}", e);
+                            process::exit(1);
+                        })
+                        .filter_map(Result::ok)
+                        .collect::<Vec<_>>()
+                } else {
+                    vec![PathBuf::from(s)]
+                }
+            })
+            .flatten()
+            .map(|p| self.calc_size(&p))
             .sum()
     }
 
     #[cfg(not(windows))]
     pub fn calculate(&self) -> u64 {
         if self.files.is_empty() {
-            return calc_dir("./", self.ignore_hidden);
+            return self.calc_dir("./");
         }
-        self.files
-            .iter()
-            .map(|x| calc_size(&x[..], self.ignore_hidden))
+        self.files.iter().map(|x| self.calc_size(x)).sum()
+    }
+
+    fn calc_dir(&self, name: impl AsRef<Path>) -> u64 {
+        WalkDir::new(name)
+            .follow_links(self.follow_links)
+            .into_iter()
+            .filter_entry(|e| !self.ignore_hidden || !is_hidden(&e.file_name()))
+            .filter_map(|r| {
+                r.map_err(|e| {
+                    eprintln!(
+                        "error accessing {}: {}",
+                        e.path().unwrap_or_else(|| Path::new("")).display(),
+                        &e
+                    );
+                })
+                .ok()
+            })
+            .filter_map(|x| {
+                x.metadata()
+                    .map_err(|e| {
+                        eprintln!("error reading metadata of {}: {}", x.path().display(), &e);
+                    })
+                    .map(|md| md.len())
+                    .ok()
+            })
             .sum()
+    }
+
+    fn calc_size(&self, name: impl AsRef<Path>) -> u64 {
+        if self.follow_links {
+            fs::metadata(name.as_ref())
+        } else {
+            fs::symlink_metadata(name.as_ref())
+        }
+        .map(|md| {
+            if md.is_dir() {
+                self.calc_dir(name.as_ref())
+            } else {
+                md.len()
+            }
+        })
+        .unwrap_or_else(|e| {
+            eprintln!("error accessing {}: {}", name.as_ref().display(), &e);
+            process::exit(2);
+        })
     }
 }
 
-fn is_hidden(s: &str) -> bool {
-    if s == "." || s == "./" || s == ".\\" {
-        false
-    } else {
-        s.starts_with('.')
-    }
+fn is_hidden(p: impl AsRef<Path>) -> bool {
+    p.as_ref().file_name().map_or(false, |s| {
+        s.to_str().map(|s| s.starts_with('.')).unwrap_or(false)
+    })
+}
+
+fn is_glob(s: &str) -> bool {
+    s.contains('*') || s.contains('?') || (s.contains('[') && s.contains(']'))
 }
